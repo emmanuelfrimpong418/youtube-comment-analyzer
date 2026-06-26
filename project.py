c4aeaac28cc429dd3f9b1c81d66fe545d0e84ef2
emmanuelfrimpong418 <emmanuelfrimpong418@gmail.com>
2026-06-26 13:20:48 +0000

    Add fetch_comments

--- project.py
+++ project.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qs
+from googleapiclient.discovery import build
+
+load_dotenv()
 
 def main():
     pass
@@ -10,6 +15,16 @@ def extract_video_id(url):
     else:
         video_id = parse_qs(result.query)["v"][0]
     return video_id
+
+def fetch_comments(video_id):
+    comments = []
+    api_key = os.environ.get("YOUTUBE_API_KEY")
+    youtube = build("youtube", "v3", developerKey=api_key)
+    response = youtube.commentThreads().list(part="snippet", videoId=video_id, textFormat="plainText").execute()
+    for comment in  response["items"]:
+        comments.append(comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"])
+    return comments
+
 
 
 if __name__ == "__main__":
