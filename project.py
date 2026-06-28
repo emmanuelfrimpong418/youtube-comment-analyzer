2584d94ebb596437b4ba2b327d9f3aa7001ad73a
emmanuelfrimpong418 <emmanuelfrimpong418@gmail.com>
2026-06-28 15:00:20 +0000

    Add word_frequency function with stop-word and emoji filtering

--- project.py
+++ project.py
@@ -1,8 +1,51 @@
 import os
 import sqlite3
+import string
 from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qs
 from googleapiclient.discovery import build
+
+STOP_WORDS = {"a", "an", "the",
+    "and", "or", "but", "if", "then", "else", "so", "nor",
+    "is", "am", "are", "was", "were", "be", "been", "being",
+    "i", "you", "he", "she", "it", "we", "they",
+    "me", "him", "her", "us", "them",
+    "my", "your", "his", "its", "our", "their",
+    "mine", "yours", "ours", "theirs",
+    "this", "that", "these", "those",
+    "to", "of", "in", "on", "at", "for", "with", "about", "against",
+    "between", "into", "through", "during", "before", "after",
+    "above", "below", "from", "up", "down", "out", "off", "over", "under",
+    "again", "further", "once",
+    "as", "than", "too", "very", "just", "not", "no",
+    "do", "does", "did", "doing",
+    "have", "has", "had", "having",
+    "can", "could", "will", "would", "shall", "should", "may", "might", "must",
+    "what", "which", "who", "whom", "where", "when", "why", "how",
+    "all", "any", "both", "each", "few", "more", "most",
+    "other", "some", "such", "only", "own", "same",
+    "there", "here", "now", "also"
+}
+
+YOUTUBE_NOISE_WORDS = {"video", "videos", "channel", "subscribe", "subscribed", "subscriber",
+    "subscribers", "comment", "comments", "like", "likes", "liked",
+    "dislike", "dislikes", "watch", "watching", "watched",
+    "youtube", "viewer", "viewers", "content", "creator",
+    "lol", "lmao", "omg", "wow", "yeah", "ok", "okay", "hey",
+    "first", "second", "third", "love", "im", "thats", "thank", "thanks",
+    "please", "pls", "really", "actually", "literally"
+}
+
+CONTRACTIONS = {
+    "don't", "doesn't", "didn't", "isn't", "aren't", "wasn't", "weren't",
+    "haven't", "hasn't", "hadn't", "won't", "wouldn't", "can't", "couldn't",
+    "shouldn't", "mustn't", "shan't",
+    "i'm", "you're", "he's", "she's", "it's", "we're", "they're",
+    "i've", "you've", "we've", "they've",
+    "i'll", "you'll", "he'll", "she'll", "we'll", "they'll",
+    "i'd", "you'd", "he'd", "she'd", "we'd", "they'd",
+    "that's", "there's", "what's", "who's", "let's",
+}
 
 load_dotenv()
 
@@ -69,6 +112,22 @@ def top_comments(limit, video_id):
     results = res.fetchall()
     con.close()
     return results
+
+def word_frequency(video_id):
+    con = sqlite3.connect("video_data.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT comment FROM comments WHERE video_id = ?", (video_id,))
+    results = res.fetchall()
+    word_count = {}
+    for result in results:
+        for word in result[0].split():
+            word = word.lower().strip(string.punctuation)
+            if (word not in STOP_WORDS and word not in YOUTUBE_NOISE_WORDS and word not in CONTRACTIONS
+                    and any(char.isalpha() for char in word)):
+                word_count[word] = word_count.get(word, 0) + 1
+    con.close()
+    sorted_words = sorted(word_count.items(), key=lambda pair: pair[1], reverse=True)
+    return dict(sorted_words)
 
 
 
