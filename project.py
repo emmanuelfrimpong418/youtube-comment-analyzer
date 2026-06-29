8045c1f53dcd1d7605b7cd29626fcc5cda102cf1
emmanuelfrimpong418 <emmanuelfrimpong418@gmail.com>
2026-06-29 23:49:24 +0000

    Move stop words and constants to constants.py

--- project.py
+++ project.py
@@ -4,48 +4,7 @@ import string
 from dotenv import load_dotenv
 from urllib.parse import urlparse, parse_qs
 from googleapiclient.discovery import build
-
-STOP_WORDS = {"a", "an", "the",
-    "and", "or", "but", "if", "then", "else", "so", "nor",
-    "is", "am", "are", "was", "were", "be", "been", "being",
-    "i", "you", "he", "she", "it", "we", "they",
-    "me", "him", "her", "us", "them",
-    "my", "your", "his", "its", "our", "their",
-    "mine", "yours", "ours", "theirs",
-    "this", "that", "these", "those",
-    "to", "of", "in", "on", "at", "for", "with", "about", "against",
-    "between", "into", "through", "during", "before", "after",
-    "above", "below", "from", "up", "down", "out", "off", "over", "under",
-    "again", "further", "once",
-    "as", "than", "too", "very", "just", "not", "no",
-    "do", "does", "did", "doing",
-    "have", "has", "had", "having",
-    "can", "could", "will", "would", "shall", "should", "may", "might", "must",
-    "what", "which", "who", "whom", "where", "when", "why", "how",
-    "all", "any", "both", "each", "few", "more", "most",
-    "other", "some", "such", "only", "own", "same",
-    "there", "here", "now", "also"
-}
-
-YOUTUBE_NOISE_WORDS = {"video", "videos", "channel", "subscribe", "subscribed", "subscriber",
-    "subscribers", "comment", "comments", "like", "likes", "liked",
-    "dislike", "dislikes", "watch", "watching", "watched",
-    "youtube", "viewer", "viewers", "content", "creator",
-    "lol", "lmao", "omg", "wow", "yeah", "ok", "okay", "hey",
-    "first", "second", "third", "love", "im", "thats", "thank", "thanks",
-    "please", "pls", "really", "actually", "literally"
-}
-
-CONTRACTIONS = {
-    "don't", "doesn't", "didn't", "isn't", "aren't", "wasn't", "weren't",
-    "haven't", "hasn't", "hadn't", "won't", "wouldn't", "can't", "couldn't",
-    "shouldn't", "mustn't", "shan't",
-    "i'm", "you're", "he's", "she's", "it's", "we're", "they're",
-    "i've", "you've", "we've", "they've",
-    "i'll", "you'll", "he'll", "she'll", "we'll", "they'll",
-    "i'd", "you'd", "he'd", "she'd", "we'd", "they'd",
-    "that's", "there's", "what's", "who's", "let's",
-}
+from constants import STOP_WORDS, YOUTUBE_NOISE_WORDS, CONTRACTIONS
 
 load_dotenv()
 
